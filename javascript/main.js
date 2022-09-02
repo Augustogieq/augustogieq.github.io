8b4fc448cf5093894a9abe4fc211c629d1872d39
Augusto Giequelin Neto <51974657+Augustogieq@users.noreply.github.com>
2022-09-01 23:40:03 -0300

    Delete javascript directory

--- javascript/main.js
+++
@@ -1,9 +0,0 @@
-/**
- Contador de Cliques
- */
-var button = document.getElementById("clickme"),
-  count = 0;
-button.onclick = function() {
-  count += 1;
-  button.innerHTML = "Clique: " + count;
-};
